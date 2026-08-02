a1e9f2def512ed8e9d0d9a32c19fa054f1c57117
anzrro <aaronzrom@gmail.com>
2026-08-02 23:38:44 +0800

    refactor: for css

--- view/includes/header.php
+++ view/includes/header.php
@@ -6,10 +6,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Green Flag</title>
 
-    <link rel="stylesheet" href="../view/css/global.css">
-    <link rel="stylesheet" href="../view/css/home.css">
-    <link rel="stylesheet" href="../view/css/about.css">
-    <link rel="stylesheet" href="../view/css/responsive.css">
+    <link rel="stylesheet" href="../view/css/global.css?v=<?php echo time(); ?>">
+    <link rel="stylesheet" href="../view/css/home.css?v=<?php echo time(); ?>">
+    <link rel="stylesheet" href="../view/css/about.css?v=<?php echo time(); ?>">
+    <link rel="stylesheet" href="../view/css/responsive.css?v=<?php echo time(); ?>">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css">
 </head>
 <body>
