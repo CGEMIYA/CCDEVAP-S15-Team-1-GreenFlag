839bf15b3c1d0bf2fb07238bf3c3f3393c1e17ae
CGEMIYA <percyvalschneider@gmail.com>
2026-08-03 23:33:37 +0800

    Added a class that indicates if page is active and highlights accordingly

--- view/admin/includes/sidebar.php
+++ view/admin/includes/sidebar.php
@@ -1,3 +1,8 @@
+<?php 
+    // Detects the file name of the currently active page (e.g., 'users.php', 'spots.php')
+    $current_page = basename($_SERVER['PHP_SELF']); 
+?>
+
 <div class="sidebar">
     <div class="sidebar-header">
         <a href="../../index.php">
@@ -7,23 +12,23 @@
     </div>
 
     <nav>
-        <a href="analytics.php">
+        <a href="analytics.php" class="<?= ($current_page == 'analytics.php') ? 'active' : '' ?>">
             <i class="fa-solid fa-chart-line"></i>
             Dashboard
         </a>
-        <a href="users.php">
+        <a href="users.php" class="<?= ($current_page == 'users.php') ? 'active' : '' ?>">
             <i class="fa-solid fa-users"></i>
             Users
         </a>
-        <a href="spots.php">
+        <a href="spots.php" class="<?= ($current_page == 'spots.php') ? 'active' : '' ?>">
             <i class="fa-solid fa-location-dot"></i>
             Spots
         </a>
-        <a href="tags.php">
+        <a href="tags.php" class="<?= ($current_page == 'tags.php') ? 'active' : '' ?>">
             <i class="fa-solid fa-tags"></i>
             Tags
         </a>
-        <a href="../aboutus.php">
+        <a href="../aboutus.php" class="<?= ($current_page == 'aboutus.php') ? 'active' : '' ?>">
             <i class="fa-solid fa-circle-info"></i>
             About Us
         </a>
